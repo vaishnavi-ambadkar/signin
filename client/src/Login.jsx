52d1e8946d6eb263726b96a2773929c1e8be2291
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-01-20 18:38:26 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -4,13 +4,13 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-   axios.defaults.withCredentials = true;
+  
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-
+    axios.defaults.withCredentials = true;
     const handleSubmit = (e) => {
         e.preventDefault();
         axios
